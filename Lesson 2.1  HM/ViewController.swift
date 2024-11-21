d71a6d797e449259e8053743ac51b31f9c0624f9
Orekov Anton <orekhovant@icloud.com>
2024-11-21 13:17:28 +0500

    Initial Commit

---
+++ Lesson 2.1  HM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.1  HM
+//
+//  Created by Антон Орехов on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
